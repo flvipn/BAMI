51b5e3ce6d4bb1b9754c7b50e4aa2e7e1ae03bc5
Flaviu <pauliepana92@gmail.com>
2025-12-15 19:39:35 +0200

    laborator 1 rezolvat complet

---
+++ Iepan_Flaviu_Lab1/Controllers/MessageController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Iepan_Flaviu_Lab1.Controllers
+{
+    public class MessageController : Controller
+    {
+        public ActionResult<string> Index()
+        {
+            return "Welcome user!";
+        }
+        public ActionResult<string> WelcomeWithHello()
+        {
+            return "Hello and welcome user!"; 
+        }
+        public ActionResult<string> NameText(string text, int number)
+        {
+            return $"Hello {text}, your number is: {number}!";
+        }
+    }
+}
